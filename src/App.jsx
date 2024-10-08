b749fd08df08d4123ca7231453b5af1e4d27e568
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-08 15:14:22 +0530

    install react and tailwindcss for frontend

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-xl bg-red-500 p-4 text-center">
+      This is the main Application
+    </div>
+  );
+};
+
+export default App;

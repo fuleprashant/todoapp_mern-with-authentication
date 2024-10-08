e7114a6c6a493cfe181422fb5195b750df3acc44
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-08 15:39:39 +0530

    craete a server

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+
+app.use("/", (req, res) => {
+  res.send("The server has been start now on the http//:localhost:4000");
+});
 app.listen(port, () => {
   console.log(`The server is start on the port ${port}`);
 });

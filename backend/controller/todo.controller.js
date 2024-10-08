2745c87f63ec3bc7a96ca2eb1d31675728925171
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-08 17:09:08 +0530

    create a router for the todo

--- backend/controller/todo.controller.js
+++ backend/controller/todo.controller.js
@@ -1,13 +1,13 @@
 import Todo from "../model/todo.model";
 
-export const createTodo = (req, res) => {
+export const createTodo = async (req, res) => {
   const todo = new Todo({
     text: req.body.text,
     completed: req.body.completed,
   });
 
   try {
-    const newTodo = todo.save();
+    const newTodo = await todo.save();
     res.status(201).json({ message: "Todo created succesfully", newTodo });
   } catch (error) {
     console.log(error);

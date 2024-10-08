fa65aca9f182b2b1eb559adce41475f8013a1141
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-08 18:44:42 +0530

    add update and delete function

--- backend/controller/todo.controller.js
+++ backend/controller/todo.controller.js
@@ -1,5 +1,6 @@
 import Todo from "../model/todo.model.js";
 
+// This is the create todo
 export const createTodo = async (req, res) => {
   const { text, completed } = req.body;
 
@@ -18,12 +19,53 @@ export const createTodo = async (req, res) => {
   }
 };
 
+// this is the function where we get all todos
+
 export const getTodo = async (req, res) => {
   try {
     const todos = await Todo.find();
     res.status(201).json({ message: "Todo fetched succesfully", todos });
   } catch (error) {
     console.log(error);
-    res.status(400).json({ message: "Error occuring while creating Todo" });
+    res.status(400).json({ message: "Error occuring while get all  Todo" });
+  }
+};
+
+// this is the update fucntion
+export const updateTodo = async (req, res) => {
+  const { id } = req.params;
+  const { text, completed } = req.body;
+  try {
+    const todo = await Todo.findByIdAndUpdate(
+      id,
+      { text, completed },
+      { new: true }
+    );
+    console.log(text);
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+    res.status(200).json({ message: "Todo updated successfully", todo });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: "Error occuring while updating Todo" });
+  }
+};
+
+// this is the delete function
+export const deleteTodo = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedTodo = await Todo.findByIdAndDelete(id);
+
+    if (!deletedTodo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    res.status(200).json({ message: "Todo deleted successfully", deletedTodo });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Error occurred while deleting Todo" });
   }
 };
--- backend/routes/todo.route.js
+++ backend/routes/todo.route.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { createTodo, getTodo } from "../controller/todo.controller.js";
+import {
+  createTodo,
+  deleteTodo,
+  getTodo,
+  updateTodo,
+} from "../controller/todo.controller.js";
 
 const router = express.Router();
 
 router.post("/create", createTodo);
 router.get("/gettodo", getTodo);
+router.put("/updatetodo/:id", updateTodo);
+router.delete("/delete/:id", deleteTodo);
 
 export default router;

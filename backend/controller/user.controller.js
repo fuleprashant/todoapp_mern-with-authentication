a3a8082fe34aaf986d6bdf57d268ba9477a63d9b
Prashant Fule <tbs.prasanth@gmail.com>
2024-10-09 11:01:45 +0530

    add the register function

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,5 +1,25 @@
-export const SignUp = (req, res) => {
-  console.log("this is signUp function");
+import user from "../model/user.model.js";
+
+export const SignUp = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    // console.log(username);
+    // console.log(email);
+    // console.log(password);
+    const User = await user.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User already registerd" });
+    }
+
+    const newUser = new user({ username, email, password });
+    await newUser.save();
+    if (newUser) {
+      res.status(201).json({ message: "User registered succesfully", newUser });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error to registered the user" });
+  }
 };
 
 export const Login = (req, res) => {
